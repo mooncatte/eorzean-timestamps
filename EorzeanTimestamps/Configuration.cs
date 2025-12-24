eb7879d8a798a2165ae2473d850c256c1fc9d424
rory mooncatte <222881520+mooncatte@users.noreply.github.com>
2025-12-24 16:35:56 -0500

    Modify placeholders from template

--- EorzeanTimestamps/Configuration.cs
+++ EorzeanTimestamps/Configuration.cs
@@ -1,7 +1,7 @@
 ﻿using Dalamud.Configuration;
 using System;
 
-namespace SamplePlugin;
+namespace EorzeanTimestamps;
 
 [Serializable]
 public class Configuration : IPluginConfiguration
--- EorzeanTimestamps/Plugin.cs
+++ EorzeanTimestamps/Plugin.cs
@@ -4,9 +4,9 @@ using Dalamud.Plugin;
 using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
-using SamplePlugin.Windows;
+using EorzeanTimestamps.Windows;
 
-namespace SamplePlugin;
+namespace EorzeanTimestamps;
 
 public sealed class Plugin : IDalamudPlugin
 {
@@ -22,7 +22,7 @@ public sealed class Plugin : IDalamudPlugin
 
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    public readonly WindowSystem WindowSystem = new("EorzeanTimestamps");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
 
